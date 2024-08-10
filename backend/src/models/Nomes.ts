b9e08994a616dd0a1ea448d7b821d33039e87710
leonardo sarda <leonardo.sarda@icloud.com>
2024-08-10 20:35:17 -0300

    Feito a model de nomes

---
+++ backend/src/models/Nomes.ts
@@ -0,0 +1,13 @@
+class Nomes {
+  name: string;
+
+  // constructor({ name }: Omit<Nomes>) {
+  //   this.name = name;
+  // }
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+export default Nomes;
